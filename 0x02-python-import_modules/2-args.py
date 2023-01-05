3abf38d9bf3bdb9c1f61bb3dba293919f4f368d5
Daniel Okosun <okosunsunogieva@gmail.com>
2023-01-05 13:04:14 -0800

    Corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ i = 1
 
 if __name__ == '__main__':
 	if len(argv) == 1:
-    		print('0 arguments.')
-    	elif len(argv) == 2:
+		print('0 arguments.')
+	elif len(argv) == 2:
 		print('1 argument:')
 		print('1: {}'.format(argv[1]))
 	else:
