3c5b064b03bec6db21ed702ce333595496e8f354
Daniel Okosun <okosunsunogieva@gmail.com>
2023-01-11 14:35:15 -0800

    Updated Repo

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-	return [replace if my_list[i] == search else my_list[i]
-            for i in range(len(my_list))]
+    return [replace if search == n else n for n in my_list]
