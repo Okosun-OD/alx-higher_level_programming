06a15330d0711f82313983fc3720c1c0c65650ae
Daniel Okosun <okosunsunogieva@gmail.com>
2023-02-07 09:22:28 -0800

    More

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
+"""add item"""
+
+
 import json
-"""
-This module contains a function that
-writes an Object to a text file, using
-a JSON representation.
+import sys
+import os.path
 
-"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def save_to_json_file(my_obj, filename):
-    """
-    save_to_json_file- Writes an Object to a text file,
-    using a JSON representation.
-
-    Args:
-        my_obj(str): stream object
-        filename(str): filename
-
-    Returns:
-        Nothing.
-
-    """
-    with open(filename, 'w+') as f:
-        json.dump(my_obj, f)
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
