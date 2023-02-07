0e3d1d694340e98eb6137af2abd0f3bd073b7609
Daniel Okosun <okosunsunogieva@gmail.com>
2023-02-06 20:36:46 -0800

    Added files

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """
-This module implements a custom list object
+This module has a class MyList that inherits from
+list. Has a public instance method print_sorted that
+prints the list in ascending order
+
 """
 
 
 class MyList(list):
-    """Custom List
+    """MyList class subclass of list
+    Attributes:
+    print_sorted: prints the list in a sorted fashion
     """
+
     def print_sorted(self):
-        """
-        prints the list, but sorted (ascending sort)
-        """
+        """print_sorted method prints list sorted"""
         print(sorted(self))
