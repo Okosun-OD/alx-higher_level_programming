e5b1924f45eea9fe24d366f4d9073790c65816e8
Daniel Okosun <okosunsunogieva@gmail.com>
2023-02-07 09:15:17 -0800

    Correction to add. task 1

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
+"""json"""
 
 
 class Student:
-    """
-    Student Class
-
-    """
+    """A student."""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Object constructor
-
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """
-        Returns a dictionary representation of a student
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance."""
+        if attrs is not None and all(isinstance(x, str) for x in attrs):
+            d = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    d[k] = v
+            return d
+        else:
+            return self.__dict__
 
-        """
-        return self.__dict__
+    def reload_from_json(self, json):
+        """reload data from json"""
+        for (key, value) in json.items():
+            setattr(self, key, value)
